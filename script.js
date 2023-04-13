13ef945ba9669125d7021c888a8d7c12ba605aa4
Willian Carvalho <72278301+Williams26-master@users.noreply.github.com>
2023-04-13 16:15:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//Criando a função
+//function escrever () {
+    //let nome = "william"
+    //let idade = 19
+  //  console.log(`Meu nome é ${nome} e minha idade é ${idade}`)
+//}
+//chamando a função
+//escrever()
+
+//Função com parametros
+let nome = 'william'
+let idade = 19 
+
+function escrever (nome, idade) {
+    console.log(`Meu nome é ${nome} e minha idade é ${idade}`)
+  
+}
+
+escrever(nome,idade)
